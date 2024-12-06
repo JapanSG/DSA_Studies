b13d8e09eb2ada8ab66c8f426106870d062f0957
JapanSG <japan.sri45326@gmail.com>
2024-12-06 12:45:57 +0700

    Modified linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -13,9 +13,9 @@ class LinkedList:
     '''LinkedList Class'''
     def __init__(self, *val) -> None:
         '''Constructor'''
-        self._head = Node(val[0])
-        pointer = self._head
-        self._pointer = self._head
+        self.__head = Node(val[0])
+        pointer = self.__head
+        self.__pointer = self.__head
         for i in range(1 ,len(val)):
             pointer.next = Node(val[i])
             pointer = pointer.next
@@ -23,7 +23,7 @@ class LinkedList:
     def __str__(self) -> str:
         '''__str__'''
         string = "["
-        pointer = self._head
+        pointer = self.__head
         while pointer:
             string += f" {pointer.val}"
             pointer = pointer.next
@@ -33,7 +33,7 @@ class LinkedList:
     def __len__(self) -> int:
         '''Return lenght of LinkeList'''
         length = 0
-        pointer = self._head
+        pointer = self.__head
         while pointer:
             pointer = pointer.next
             length += 1
@@ -41,20 +41,17 @@ class LinkedList:
 
     def __add__(self, lis):
         '''Add Function'''
-        new1 = self.clone()
-        new2 = lis.clone()
-        pointer = new1._head
-        while pointer.next:
-            pointer = pointer.next
-        pointer.next = new2._head
-        return new1
+        new = self.clone()
+        for i in lis:
+            new.append(i)
+        return new
 
     def __getitem__(self, index : int):
         '''Get val from Node at index'''
         if index < 0:
             index += len(self)
         i = 0
-        pointer = self._head
+        pointer = self.__head
         while i != index:
             pointer = pointer.next
             i += 1
@@ -63,7 +60,7 @@ class LinkedList:
     def __setitem__(self, index : int, val):
         '''Set val in Node at index'''
         i = 0
-        pointer = self._head
+        pointer = self.__head
         while i != index:
             pointer = pointer.next
             i += 1
@@ -71,23 +68,23 @@ class LinkedList:
 
     def __iter__(self):
         '''Return iterator of self'''
-        self._pointer = self._head
+        self.__pointer = self.__head
         return self
 
     def __next__(self):
         '''Logic for iterator'''
-        if self._pointer:
-            val = self._pointer.val
-            self._pointer = self._pointer.next
+        if self.__pointer:
+            val = self.__pointer.val
+            self.__pointer = self.__pointer.next
             return val
         raise StopIteration
 
     def clone(self):
         '''Return a clone of LinkedList'''
-        if not self._head:
+        if not self.__head:
             return None
-        new = LinkedList(self._head.val)
-        pointer = self._head.next
+        new = LinkedList(self.__head.val)
+        pointer = self.__head.next
         while pointer:
             new.append(pointer.val)
             pointer = pointer.next
@@ -95,19 +92,19 @@ class LinkedList:
 
     def append(self, val) -> None:
         '''Append Node'''
-        pointer = self._head
+        pointer = self.__head
         while pointer.next:
             pointer = pointer.next
         pointer.next = Node(val)
 
     def pop(self):
         '''Remove last Node'''
-        pointer = self._head
+        pointer = self.__head
         if not pointer:
             raise ValueError("Can't pop an Empty List")
         if not pointer.next:
-            val = self._head.val
-            self._head = None
+            val = self.__head.val
+            self.__head = None
             return val
         while pointer.next.next:
             pointer = pointer.next
@@ -118,8 +115,12 @@ class LinkedList:
 def main():
     '''Driver Code'''
     lis = LinkedList(10,20,30,40,50)
-    lis1 = LinkedList(60,70,80)
-    lis2 = lis +lis1
+    lis1 = LinkedList(23,34,45)
+    for i in lis:
+        print(i)
+    lis2 = lis + lis1
     print(lis2)
+    print(lis2[2])
+
 if __name__ == "__main__":
     main()
