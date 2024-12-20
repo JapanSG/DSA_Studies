4b3d0f7caaef85a607935897c8177c296d745609
JapanSG <japan.sri45326@gmail.com>
2024-12-20 11:03:27 +0700

    Remove unneccessary code

--- Week1/Lab01-04.py
+++ Week1/Lab01-04.py
@@ -3,7 +3,7 @@ def bundle_student():
     '''bundle all student info into a list and return the list'''
     student = []
     for _ in range(5):
-        student.append(input().strip())
+        student.append(input())
     return student
 
 def print_student(student : list):
@@ -11,14 +11,14 @@ def print_student(student : list):
     honorific = "Mr"
     if student[1] == "Female":
         honorific = "Miss"
-    print(f"{honorific} {student[0]} ({student[2]}) ID: {student[3]} GPA {float(student[4]):.2f}".strip())
+    print(f"{honorific} {student[0]} ({student[2]}) ID: {student[3]} GPA {float(student[4]):.2f}")
 
 def main():
     '''Driver Code'''
     students = []
     for _ in range(3):
         students.append(bundle_student())
-    stu_id = input().strip()
+    stu_id = input()
     for student in students:
         if stu_id == student[3]:
             print_student(student)
